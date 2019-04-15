78f798f1d0f4e57b1d9cde05f15713d14c24d9f9
Ari Sanguinetti <hello@arielsanguinetti.com.ar>
2019-04-15 20:44:41 -0300

    Add MongoDB

--- cron.js
+++ cron.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron');
 const moment = require('moment-timezone');
-const { runCron } = require('./scraper');
+const { connectDB } = require('./lib/db');
+const { runCron } = require('./lib/scraper');
 
 moment.locale('es');
 
@@ -14,8 +15,12 @@ const doCron = () => {
   }
 };
 
-// Lun-Vie de 9 a 15
-cron.schedule('0 10-15 * * 1,2,3,4,5', doCron, {
-  scheduled: true,
-  timezone: 'America/Argentina/Buenos_Aires',
-});
+connectDB()
+  .then(() => {
+    cron.schedule('0 10-15 * * 1,2,3,4,5', doCron, {
+      scheduled: true,
+      timezone: 'America/Argentina/Buenos_Aires',
+    });
+    runCron();
+  })
+  .catch(err => console.log(err));
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./lib/db');
-// require('./lib/cron');
 const asyncHandler = require('./lib/asyncHandler');
 const { UvaModel, UsdModel, UsdUvaModel } = require('./lib/models');
 const { uniqueCount, uniqueCountObjects } = require('./lib/utils');
@@ -15,11 +14,10 @@ app.get(
   asyncHandler(async (req, res, next) => {
     const uvaValues = await UvaModel.getUnique();
     const usdValues = await UsdModel.getUnique();
-    const usdUvaValues = await UsdUvaModel.getUnique();
+    const usdUva = await UsdUvaModel.getUnique();
 
     const uva = uniqueCount(uvaValues);
     const usd = uniqueCountObjects(usdValues);
-    const usdUva = uniqueCountObjects(usdUvaValues);
 
     return res.json({
       uva,
@@ -32,9 +30,9 @@ app.get(
 const PORT = process.env.port || 2093;
 
 connectDB()
-  .then(() =>
+  .then(() => {
     app.listen(PORT, () => {
       console.log(`App running on http://localhost:${PORT}`);
-    })
-  )
+    });
+  })
   .catch(err => console.log(err));
